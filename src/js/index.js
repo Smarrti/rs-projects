10ad125186aca81cfb6dffd14646c206fdddfd25
Sergey <bashkin2013@mail.ru>
2020-04-13 22:45:13 +0400

    feat: add functionality for menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 import '../css/style.scss';
+
+const burgerButton = document.querySelector('.hamburger-menu');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+
+function moveSidebar() {
+    const hamburgerButton = document.querySelector('.hamburger');
+    const sidebar = document.querySelector('.sidebar');
+    hamburgerButton.classList.toggle('hamburger_active');
+    sidebarWrapper.classList.toggle('sidebar-wrapper_active');
+    sidebar.classList.toggle('sidebar_active');
+}
+
+burgerButton.addEventListener('click', moveSidebar);
+sidebarWrapper.addEventListener('click', moveSidebar);
