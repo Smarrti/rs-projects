308e9a0eafdd866860438b2d35131e26beabd8f0
Sergey <bashkin2013@mail.ru>
2020-04-23 00:10:47 +0400

    refactor: code changed with eslint

--- english-for-kids/src/js/Dictionary.js
+++ english-for-kids/src/js/Dictionary.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export const cards = [ 
   ['Action (set A)', 'Action (set B)', 'Animal (set A)', 'Animal (set B)', 'Clothes', 'Emotions', 'Rooms in a house', 'Transports'],
   [
