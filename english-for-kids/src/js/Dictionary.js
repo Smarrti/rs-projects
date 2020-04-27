647ee1766a3b88fcf8b307c98cf4557e66e4a68f
Sergey <bashkin2013@mail.ru>
2020-04-27 16:04:43 +0400

    refactor: splited a line into several lines

--- english-for-kids/src/js/Dictionary.js
+++ english-for-kids/src/js/Dictionary.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/prefer-default-export
 export const cards = [ 
-  ['Action (set A)', 'Action (set B)', 'Animal (set A)', 'Animal (set B)', 'Clothes', 'Emotions', 'Rooms in a house', 'Transports'],
+  ['Action (set A)', 'Action (set B)', 'Animal (set A)',
+   'Animal (set B)', 'Clothes', 'Emotions',
+    'Rooms in a house', 'Transports'
+  ],
   [
     {
       word: 'cry',
