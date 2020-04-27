1305a530d902baa53545d232c6a3aeb84980ee9e
Sergey <bashkin2013@mail.ru>
2020-04-27 16:49:04 +0400

    fix: changed paths of pictires and sounds

--- english-for-kids/src/js/Constatnt.js
+++ english-for-kids/src/js/Constatnt.js
@@ -1,9 +1,9 @@
 export const placeMainHtmlFile = './index.html';
-export const failureImg = '../assets/img/failure.jpg';
-export const successImg = '../assets/img/success.jpg';
+export const failureImg = './assets/img/failure.jpg';
+export const successImg = './assets/img/success.jpg';
 export const timeMessageOnGameEnd = 5000;
-export const correctSound = '../assets/audio/correct.mp3';
-export const errorSound = '../assets/audio/error.mp3';
-export const failureSound = '../assets/audio/failure.mp3';
-export const successSound = '../assets/audio/success.mp3';
+export const correctSound = './assets/audio/correct.mp3';
+export const errorSound = './assets/audio/error.mp3';
+export const failureSound = './assets/audio/failure.mp3';
+export const successSound = './assets/audio/success.mp3';
 export const timeOfSuccessSoundAndVoice = 400;
