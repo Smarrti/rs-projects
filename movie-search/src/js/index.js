1ada7e727a45da292d6c3cceaa21a95b78eed886
Sergey <bashkin2013@mail.ru>
2020-04-30 14:04:44 +0400

    feat: added open and close buttons for keyboard

--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -1,11 +1,15 @@
 import '../css/style.scss';
+import './swiper.min';
 import './Swiper';
 import './KeyboardDictionary';
 import { createKeyboard } from "./Keyboard";
 
 document.querySelector('body').addEventListener('click', (event) => {
+  event.preventDefault();
   const { target } = event;
-  if (target.classList.contains('search__keyboard')) {
-    createKeyboard();
+  const hasClassList = target.classList;
+  if (hasClassList.contains('search__keyboard') || hasClassList.contains('keyboard__close')) {
+    const keyboardWrapper = document.querySelector('.keyboard__wrapper');
+    keyboardWrapper.classList.toggle('keyboard__wrapper_active');
   }
 })
