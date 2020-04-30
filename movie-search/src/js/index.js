6c2c966b1f198c829daab25f65aecac3b646860b
Sergey <bashkin2013@mail.ru>
2020-04-30 13:49:38 +0400

    feat: added keyboard

--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -1,2 +1,11 @@
 import '../css/style.scss';
 import './Swiper';
+import './KeyboardDictionary';
+import { createKeyboard } from "./Keyboard";
+
+document.querySelector('body').addEventListener('click', (event) => {
+  const { target } = event;
+  if (target.classList.contains('search__keyboard')) {
+    createKeyboard();
+  }
+})
