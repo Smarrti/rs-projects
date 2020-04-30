7b598e4e43265bea842c57f326d2b13e074cda59
Sergey <bashkin2013@mail.ru>
2020-04-30 23:36:50 +0400

    feat: added getdate from api

--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -3,6 +3,17 @@ import './swiper.min';
 import './Swiper';
 import './KeyboardDictionary';
 import './Keyboard';
+import { apiKey } from './ApiKey';
+
+const textInput = document.querySelector('.search__input');
+
+function getData(textInput) {
+  fetch(`http://www.omdbapi.com/?s=${textInput}&apikey=${apiKey}`)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+  });
+}
 
 document.querySelector('body').addEventListener('click', (event) => {
   event.preventDefault();
@@ -11,5 +22,9 @@ document.querySelector('body').addEventListener('click', (event) => {
   if (hasClassList.contains('search__keyboard') || hasClassList.contains('keyboard__close')) {
     const keyboardWrapper = document.querySelector('.keyboard__wrapper');
     keyboardWrapper.classList.toggle('keyboard__wrapper_active');
+  } else if (hasClassList.contains('search__submit')) {
+    getData(textInput.value);
   }
 })
+
+//http://www.omdbapi.com/?s=harry&plot=full&apikey=90596ce5
