4f13f47983e485b7ccc7538ffb2466e3004d4592
Sergey <bashkin2013@mail.ru>
2020-06-01 11:29:07 +0400

    feat: added function for send request

--- fancy-weather/src/js/index.js
+++ fancy-weather/src/js/index.js
@@ -1,1 +1,18 @@
 import './../css/style.scss';
+
+async function sendRequest(url) {
+  let data;
+  await fetch(url)
+    .then((res) => {
+      if (res.status !== 200) {
+        return Promise.reject(new Error(res.statusText));
+      }
+      return Promise.resolve(res);
+    })
+    .then((res) => res.json())
+    .then(json => {data = json})
+    .catch(() => {
+      //Oops...
+    })
+  return data;
+}
